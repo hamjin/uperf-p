53d392ed49ce857c440896cb1aaca61cbd8ba6f1
Ham <335908093@qq.com>
2022-01-19 01:01:28 +0800

    删除救砖日志

--- initsvc_uperf.sh
+++ initsvc_uperf.sh
@@ -6,31 +6,30 @@
 USER_PATH="/data/media/0/yc/uperf"
 BASEDIR="$(dirname $(readlink -f "$0"))"
 
-wait_until_login()
-{
+wait_until_login() {
     # in case of /data encryption is disabled
     while [ "$(getprop sys.boot_completed)" != "1" ]; do
         sleep 1
     done
-    
+
     # we doesn't have the permission to rw "/sdcard" before the user unlocks the screen
     local test_file="/sdcard/Android/.PERMISSION_TEST"
-    true > "$test_file"
+    true >"$test_file"
     while [ ! -f "$test_file" ]; do
-        true > "$test_file"
+        true >"$test_file"
         sleep 1
     done
     rm "$test_file"
-    
+
 }
 
-crash_recuser()
-{
+crash_recuser() {
     rm $BASEDIR/logcat.log
     logcat -f $BASEDIR/logcat.log &
     sleep 60
     killall logcat
     rm -f $BASEDIR/flags/.need_recuser
+    rm $BASEDIR/logcat.log
 }
 
 (crash_recuser &)
